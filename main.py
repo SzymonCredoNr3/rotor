75c7af5482ad6bc0d499e519e76426460f572db9
Szymon <sc_2004@efektivo.pl>
2022-10-30 20:11:54 +0100

    change of value of main_label added

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ from kivy.uix.button import Button
 class Rotor(BoxLayout):
     def __init__(self, **kwargs):
         super(Rotor, self ).__init__(**kwargs)
-        x = 1
+        self.degree = 0
         button1 = Button(text="<")
         button1.bind(on_press=self._button_action)
         self.add_widget(button1)
 
-        self.main_label = Label(text="0°")
+        self.main_label = Label(text=f"{self.degree}°")
         self.add_widget(self.main_label)
 
         button2 = Button(text=">")
@@ -27,10 +27,11 @@ class Rotor(BoxLayout):
             on click action of all buttons, change the value of number in this.main_label
         """
         if instanse.text == "<":
-            diraction = -1
+            direction = -1
         else:
-            diraction = 1
-        print("diala" + str(diraction))
+            direction = 1
+        self.degree += direction
+        self.main_label.text = f"{self.degree}°"
 
 class MyApp(App):
 
