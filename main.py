52ce37e14caf53e8308d1607b27b66cc4dbf7973
Szymon <sc_2004@efektivo.pl>
2022-10-30 20:06:43 +0100

    add of button event and base structure

--- main.py
+++ main.py
@@ -10,9 +10,27 @@ from kivy.uix.button import Button
 class Rotor(BoxLayout):
     def __init__(self, **kwargs):
         super(Rotor, self ).__init__(**kwargs)
-        self.add_widget(Button(text="<"))
-        self.add_widget(Label(text="0*"))
-        self.add_widget(Label(text=">"))
+        x = 1
+        button1 = Button(text="<")
+        button1.bind(on_press=self._button_action)
+        self.add_widget(button1)
+
+        self.main_label = Label(text="0°")
+        self.add_widget(self.main_label)
+
+        button2 = Button(text=">")
+        button2.bind(on_press=self._button_action)
+
+        self.add_widget(button2)
+    def _button_action(self, instanse: Button):
+        """
+            on click action of all buttons, change the value of number in this.main_label
+        """
+        if instanse.text == "<":
+            diraction = -1
+        else:
+            diraction = 1
+        print("diala" + str(diraction))
 
 class MyApp(App):
 
